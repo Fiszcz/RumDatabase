12704cfbf409209a1b088118b3d7b6ab3d6e0bfe
matsyk95 <matsyk998@gmail.com>
2018-11-22 00:02:25 +0100

    Add getAllRums method test

---
+++ src/main/java/pl/database/rum/app/TableRumsTest.java
@@ -0,0 +1,43 @@
+package pl.database.rum.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Assert;
+import org.junit.Test;
+import pl.database.rum.entities.Producent;
+import pl.database.rum.entities.Rum;
+import pl.database.rum.init.HibernateUtil;
+
+import static org.junit.Assert.*;
+
+public class TableRumsTest {
+
+
+
+    @Test
+    public void getAllRums() {
+        TableRums tb = new TableRums();
+        Object[][] allRums = tb.getAllRums();
+        for (Object[] row : allRums) {
+            Long rumID = (Long) row[0];
+            tb.removeRumFromDatabase(rumID);
+        }
+        SessionFactory sessionFactory;
+        Session session;
+
+        sessionFactory = HibernateUtil.getSessionFactory();
+        session = sessionFactory.openSession();
+        Producent prod = new Producent("FISzCZ", "PL", 2132);
+        Rum r = new Rum("asdasd", 21, "M", 1.2, "MA", 22, prod);
+        tb.addNewRumToDatabase(r,session );
+
+        Object[][] newtable = tb.getAllRums();
+        int l = newtable.length;
+        Assert.assertEquals(l,1);
+
+    }
+
+    @Test
+    public void getRumById() {
+    }
+}
