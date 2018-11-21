b63d921b842f82ae0c524b378692a298b3e76936
trzaskom <m_trzaskowski@wp.pl>
2018-11-21 23:36:52 +0100

    Add new methods testing inserting to database

--- src/main/java/pl/database/rum/tests/CreateTest.java
+++ src/main/java/pl/database/rum/tests/CreateTest.java
@@ -4,37 +4,37 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Assert;
 import pl.database.rum.entities.Producent;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import pl.database.rum.app.*;
 import pl.database.rum.init.HibernateUtil;
 
-/**
- * Created by miki on 2018-11-21.
- */
+
 public class CreateTest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
-
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-
-    }
-
     @org.junit.Test
-    public void createProducent() {
+    public void createProducent() throws Exception {
         String name = "name";
         String country = "country";
         Integer yearFounding = 1990;
         Producent producent = new Producent(name, country,yearFounding);
-        Assert.assertSame(name, producent.getName());
-        Assert.assertSame(country, producent.getCountry());
+        Assert.assertEquals(name, producent.getName());
+        Assert.assertEquals(country, producent.getCountry());
         Assert.assertEquals(yearFounding, producent.getYearFounding());
+    }
+
+    @org.junit.Test
+    public void testAddNullAsNewProducent() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Producent producent = new Producent(null, null, null);
+
+        long id = TableProducents.addNewProducentToDatabase(producent, session);
+
+        TableProducents tableProducents = new TableProducents();
+        Producent selectResult = tableProducents.getProducentById(id).get(0);
+        tableProducents.removeProducentFromDatabase(id);
+
+        Assert.assertNotNull(selectResult);
     }
 
     @org.junit.Test
@@ -48,11 +48,8 @@ public class CreateTest {
 
         TableProducents tableProducents = new TableProducents();
         Producent selectResult = tableProducents.getProducentById(id).get(0);
-        session.close();
+        tableProducents.removeProducentFromDatabase(id);
 
-        System.out.println(producent.toString());
-        System.out.println(id);
-        System.out.println(selectResult.toString());
 
         Assert.assertEquals(producent, selectResult);
     }
